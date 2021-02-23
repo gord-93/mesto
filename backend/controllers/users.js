628fd40ff42e9dce79795df51ff72a86fa0dbe4e
Ivan Gordievsky <kos1993@icloud.com>
2021-02-24 02:47:29 +0300

    controllers/users, auth.js

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -105,7 +105,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
-      res.send({ token });
+      res.send({ user, token });
     })
     .catch(() => {
       next(new UnauthorizedError('Проблема авторизации'));
