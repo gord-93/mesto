2d825c230330b6cc973fb0a6d0605b6152d6ae9c
Ivan Gordievsky <kos1993@icloud.com>
2021-02-25 04:47:58 +0300

    fix

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -11,9 +11,8 @@ router.get('/:_id', getUser);
 
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required()
-      .messages('Минимум 2 символа максимум 30 символов'),
-    about: Joi.string().min(2).max(30).messages('Минимум 2 символа максимум 30 символов'),
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30),
   }),
 }), patchUser);
 
