829d8319ccedac4c703016c41f40547a9f77142d
gord-93 <kos1993@icloud.com>
2022-11-09 15:08:35 +0300

    fix

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -6,24 +6,29 @@ function Profile(props) {
 
   return (
     <section className="profile">
+
       <div className='profile__container'>
-        <div className="profile__avatar-elements">
-          <img src={currentUser.avatar} className="profile__avatar" alt="Аватар"/>
-        <div className="profile__edit-avatar">
-            <button type="button" className="profile__edit-icon" onClick={props.onEditAvatar}></button>
+        <div className='profile__top'>
+          <div className="profile__avatar-elements">
+            <img src={currentUser.avatar} className="profile__avatar" alt="Аватар"/>
+            <button type="button" className="profile__edit-avatar" onClick={props.onEditAvatar}></button>
+          </div>
         </div>
-        </div>
+
         <div className="profile__info">
             <h1 className="profile__name">{currentUser.name}</h1>
             <p className="profile__about">{currentUser.about}</p>
             <button type="button" className="profile__edit-button" onClick={props.onEditProfile}></button>
         </div>
+
         <button type="button" className="profile__add-button" onClick={props.onAddPlace}></button>
       </div>
+
       <div className="profile__menu">
         <button type="button" className='profile__menu-button' onClick={props.handleClickAllButton}>All</button>
         <button type="button" className='profile__menu-button' onClick={props.handleClickMyButton}>My</button>
       </div>
+
     </section> 
   )
 }
